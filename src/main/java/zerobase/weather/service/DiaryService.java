658a4054fbf817aa3233cae5d25ab2f4acdc15cb
codesche <codesche@gmail.com>
2022-12-22 22:23:01 +0900

    Feat: 날씨 데이터 저장 API 작성 5
    - 날씨 일기(Diary) 엔터티 작성
    - 파싱된 데이터와 일기값을 DB에 저장
    - DB연동 가능하도록 날씨일기 repository 작성

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,10 +1,13 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DiaryRepository;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,6 +23,13 @@ public class DiaryService {
     @Value("${openweathermap.key}")
     private String apikey;
 
+    // DB 연동
+    private final DiaryRepository diaryRepository;
+
+    public DiaryService(DiaryRepository diaryRepository) {
+        this.diaryRepository = diaryRepository;
+    }
+
     public void createDiary(LocalDate date, String text) {
         // open weather map에서 날씨 데이터 가져오기
         String weatherData = getWeatherString();
@@ -28,7 +38,15 @@ public class DiaryService {
         Map<String, Object> parseWeather = parseWeather(weatherData);
 
         // 파싱된 데이터 + 일기 값 db에 넣기
+        Diary nowDiary = new Diary();
+        nowDiary.setWeather(parseWeather.get("main").toString());
+        nowDiary.setIcon(parseWeather.get("icon").toString());
+        nowDiary.setTemperature((Double)parseWeather.get("temp"));
+        nowDiary.setText(text);
+        nowDiary.setDate(date);
 
+        // DB에 저장
+        diaryRepository.save(nowDiary);
     }
 
     // OpenWeatherMap에서 데이터 받아오기
@@ -83,7 +101,8 @@ public class DiaryService {
         // 받아온 데이터 (json) 사용 가능하게 파싱
         JSONObject mainData = (JSONObject) jsonObject.get("main");                  // weather.main
         resultMap.put("temp", mainData.get("temp"));                                // main.temp
-        JSONObject weatherData = (JSONObject) jsonObject.get("weather");
+        JSONArray weatherArray = (JSONArray) jsonObject.get("weather");
+        JSONObject weatherData = (JSONObject) weatherArray.get(0);
         resultMap.put("main", weatherData.get("main"));
         resultMap.put("icon", weatherData.get("icon"));                             // weather.icon
         return resultMap;
