e5caef88484ccb9c432f29633aac0968dbf3f2f8
codesche <codesche@gmail.com>
2022-12-22 21:36:28 +0900

    Feat: 날씨 데이터 저장 API 작성 3
    - OpenWeatherMap에서 받아온 Json 데이터 파싱

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -3,6 +3,10 @@ package zerobase.weather.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
@@ -12,13 +16,43 @@ public class DiaryService {
     private String apikey;
 
     public void createDiary(LocalDate date, String text) {
-        getWeatherString();
+        // open weather map에서 날씨 데이터 가져오기
+        String weatherData = getWeatherString();
     }
 
     // OpenWeatherMap에서 데이터 받아오기
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apikey;
-        System.out.println(apiUrl);
-        return "";
+        try {
+            URL url = new URL(apiUrl);
+
+            // apiUrl을 Http형식으로 연결
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // 응답코드
+            int responseCode = connection.getResponseCode();
+            BufferedReader br;
+
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));        // 정상
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));        // Error
+            }
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            // 버퍼에 넣어두었던 데이터를 하나하나 읽어옴
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);             // StringBuilder 객체에 결과값을 쌓음
+            }
+
+            br.close();
+            return response.toString();
+
+        } catch (Exception e) {
+            return "failed to get response";
+        }
     }
 }
